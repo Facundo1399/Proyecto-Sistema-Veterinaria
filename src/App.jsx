f7a1ef249d8a7211e11fd6f1e75504237a3b405f
Franco Rearte <francomaximilianorearte@gmail.com>
2025-11-08 09:57:03 -0300

    se creo el componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './index.css';
 
 import Login  from "./components/login/Login" 
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
 
   return (
     <>
       <Login />
+     
     </>
   )
 }

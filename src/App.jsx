e85aff12299fec389daef435c5846a015a9f15d5
Franco Rearte <francomaximilianorearte@gmail.com>
2025-11-08 11:31:20 -0300

    se crearon validaciones en opcion turnos

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './index.css';
 
 import Login  from "./components/login/Login" 
 import Sidebar from './components/sidebar/Sidebar';
+import Turnos from './components/menu turnos/Turnos';
 
 function App() {
 
   return (
     <>
+
       <Login />
-     
+      
     </>
   )
 }
